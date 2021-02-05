b73bef122d9aaf84f2e48a70bbb8000ebcc01510
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-02-05 17:36:28 -0300

    Created entities

---
+++ src/BookStore.Domain/Models/Category.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace BookStore.Domain.Models
+{
+    public class Category : Entity
+    {
+        public string Name { get; set; }
+
+        // EF Relation
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
---
+++ src/BookStore.Domain/Models/Entity.cs
@@ -0,0 +1,7 @@
+namespace BookStore.Domain.Models
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+    }
+}
