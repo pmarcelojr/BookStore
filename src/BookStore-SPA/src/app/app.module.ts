a17e05ce7309e9a2f5fcdf0978d7db26b0122d4d
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-02-06 19:12:48 -0300

    Implementation structure of the application

--- src/BookStore-SPA/src/app/app.module.ts
+++ src/BookStore-SPA/src/app/app.module.ts
@@ -4,33 +4,43 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BookService } from './_services/book.service';
-import { CategoryService } from './_services/category.service';
-import { ConfirmationDialogService } from './_services/confirmation-dialog.service';
-import { CategoryComponent } from './categories/category/category.component';
-import { CategoryListComponent } from './categories/category-list/category-list.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
 import { BookComponent } from './books/book/book.component';
 import { BookListComponent } from './books/book-list/book-list.component';
+import { CategoryComponent } from './categories/category/category.component';
+import { CategoryListComponent } from './categories/category-list/category-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BookService } from './_services/book.service';
+import { CategoryService } from './_services/category.service';
+import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
+import { ConfirmationDialogService } from './_services/confirmation-dialog.service';
+import { NgbdDatepickerPopup } from './datepicker/datepicker-popup';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    NavComponent,
+    BookComponent,
+    BookListComponent,
     CategoryComponent,
     CategoryListComponent,
-    BookComponent,
-    BookListComponent
+    ConfirmationDialogComponent,
+    NgbdDatepickerPopup
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
+    AppRoutingModule,
     FormsModule,
+    HttpClientModule,
     NgbModule,
     BrowserAnimationsModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot(),
   ],
   providers: [
     BookService,
